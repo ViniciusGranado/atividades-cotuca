ae371ff7b33273d2550000f056b944dc673f152e
Vinicius Granado <vinicius.r.granado@gmail.com>
2021-05-10 21:09:41 -0300

    Added "Adivinhação" activity

---
+++ semestre-1/tecnicas-de-programacao/Adivinhacao/Adivinhacao/Jogo.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Adivinhacao
+{
+  class Jogo
+  {
+    private int _numero;
+    private int _numeroUsuarioDigitou;
+    private String _dica;
+    private int _contagemJogadas;
+
+    public Jogo ()
+    {
+      this._contagemJogadas = 0;
+    }
+    public void setNumero (int numero)
+    {
+      this._numero = numero;
+    }
+
+    public int getNumero()
+    {
+      return this._numero;
+    }
+
+    public int getNumeroUsuarioDigitou ()
+    {
+      return this._numeroUsuarioDigitou;
+    }
+
+    public int getContagemJogadas ()
+    {
+      return this._contagemJogadas;
+    }
+
+    public String getDica ()
+    {
+      return this._dica;
+    }
+
+
+    public void atualizarContagemJogadas ()
+    {
+      this._contagemJogadas++;
+    }
+
+    public void cadastrarJogada ()
+    {
+      while (true)
+      {
+        Console.Write("Digite um número de 0 a 10: ");
+        String jogada = Console.ReadLine();
+
+        int jogadaConvertida;
+        if (
+          int.TryParse(jogada, out jogadaConvertida) &&
+          (jogadaConvertida >= 0 && jogadaConvertida <= 10)
+        )
+        {
+          this._numeroUsuarioDigitou = jogadaConvertida;
+          return;
+        }
+
+        Console.WriteLine("\nJogada inválida, tente novamente.\n");
+      }
+    }
+
+    public bool verificarJogada ()
+    {
+      int diferenca = this._numero - this._numeroUsuarioDigitou;
+
+      this._dica = (diferenca >= -2 && diferenca <= 2 ?
+                    "Está quente!" :
+                    "Está frio"
+                    );
+
+      return this._numero == this._numeroUsuarioDigitou;
+    }
+  }
+}
---
+++ semestre-1/tecnicas-de-programacao/Adivinhacao/Adivinhacao/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Adivinhacao
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Random random = new Random();
+      Jogo jogoAtual = new Jogo();
+      jogoAtual.setNumero(random.Next(11));
+      bool jogadorAcertou;
+
+      Console.WriteLine("------------------------------");
+      Console.WriteLine("      JOGO DA ADIVINHAÇÃO     ");
+      Console.WriteLine("------------------------------");
+     
+      while (true)
+      {
+        jogoAtual.cadastrarJogada();
+
+        jogadorAcertou = jogoAtual.verificarJogada();
+
+        jogoAtual.atualizarContagemJogadas();
+
+        if (jogoAtual.getContagemJogadas() >= 3 || jogadorAcertou)
+        {
+          break;
+        }
+
+        Console.WriteLine("\n" + jogoAtual.getDica() + ", tente novamente!\n");
+      };
+
+      Console.WriteLine("\n-------------------------------------");
+      Console.WriteLine(
+        jogadorAcertou ?
+         "  Parabéns! Você adivinhou o número." :
+         "    Que pena, não foi dessa vez!"
+      );
+      Console.WriteLine("-------------------------------------");
+
+    }
+  }
+}
