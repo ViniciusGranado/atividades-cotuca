c18925378462807ead739cae772c7d3c2288e00d
Vinicius Granado <vinicius.r.granado@gmail.com>
2021-06-10 20:07:12 -0300

    Added exercise03

---
+++ semestre-1/tecnicas-de-programacao/avaliacao/exercicio03/exercicio03/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace exercicio03
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int numero;
+
+      Console.Write("Digite um valor inteiro: ");
+      numero = Convert.ToInt32(Console.ReadLine());
+
+      if (numero > 0)
+      {
+        Console.WriteLine("\nO número " + numero + " é positivo.");
+      } else if (numero < 0)
+      {
+        Console.WriteLine("\nO número " + numero + " é negativo.");
+      }
+      else
+      {
+        Console.WriteLine("\nO número " + numero + " é neutro.");
+      }
+    }
+  }
+}
