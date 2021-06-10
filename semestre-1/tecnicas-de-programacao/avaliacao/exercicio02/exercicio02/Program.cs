fe4cc0fa071fafff168b44cbd4a267c66db2fd34
Vinicius Granado <vinicius.r.granado@gmail.com>
2021-06-10 19:54:50 -0300

    Added Exercicio02

---
+++ semestre-1/tecnicas-de-programacao/avaliacao/exercicio02/exercicio02/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace exercicio02
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int valor1;
+      int valor2;
+
+      Console.Write("Escreva o primeiro valor: ");
+      valor1 = Convert.ToInt32(Console.ReadLine());
+
+      Console.Write("Escreva o segundo valor: ");
+      valor2 = Convert.ToInt32(Console.ReadLine());
+
+      if (valor1 > valor2)
+      {
+        Console.WriteLine("\nO maior valor digitado foi " + valor1);
+      }
+      else if (valor1 < valor2)
+      {
+        Console.WriteLine("\nO maior valor digitado foi " + valor2);
+      }
+      else
+      {
+        Console.WriteLine("\nOs valores digitados são iguais.");
+      }
+    }
+  }
+}
