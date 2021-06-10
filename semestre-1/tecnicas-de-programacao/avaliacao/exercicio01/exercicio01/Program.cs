86b7862671c689f7850d648eb47969e440ad99cc
Vinicius Granado <vinicius.r.granado@gmail.com>
2021-06-10 19:48:21 -0300

    Added Exercicio01

---
+++ semestre-1/tecnicas-de-programacao/avaliacao/exercicio01/exercicio01/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace exercicio01
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      String nome;
+      int idade;
+
+      Console.Write("Digite o seu nome: ");
+      nome = Console.ReadLine();
+
+      Console.Write("Digite a sua idade: ");
+      idade = Convert.ToInt32(Console.ReadLine());
+
+      Console.Write("\n" + nome);
+      if (idade % 2 == 0)
+      {
+        Console.WriteLine(" Aceito para o exame.");
+      }
+      else
+      {
+        Console.WriteLine(" Espere a próxima oportunidade.");
+      }
+    }
+  }
+}
