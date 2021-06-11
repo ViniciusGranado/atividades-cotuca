f989230398d6af63d25651993d44ac583646e0ff
Vinicius Granado <vinicius.r.granado@gmail.com>
2021-06-10 22:30:39 -0300

    Added exercise04

---
+++ semestre-1/tecnicas-de-programacao/avaliacao/exercise04/exercise04/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace exercise04
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      float salario;
+
+      Console.Write("Digite o seu salário: R$");
+      salario = float.Parse(Console.ReadLine());
+
+      Console.Write("\nAlíquota: ");
+      if (salario <= 22847.76)
+      {
+        Console.WriteLine("ISENTO");
+      }
+      else if (salario <= 33919.80)
+      {
+        Console.WriteLine("7,5%");
+      }
+      else if (salario <= 45012.60)
+      {
+        Console.WriteLine("15%");
+      }
+      else if (salario <= 55976.16)
+      {
+        Console.WriteLine("22,5%");
+      }
+      else
+      {
+        Console.WriteLine("27,5%");
+      }
+    }
+  }
+}
