7c07f339b7e172f2e6348931bdb7835e6a8a2a23
Vinicius Granado <vinicius.r.granado@gmail.com>
2021-06-14 16:46:36 -0300

    Added exercicio01

---
+++ semestre-1/tecnicas-de-programacao/avaliacao02/exercicio01/exercicio01/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace exercicio01
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int valorUsuario;
+
+      do
+      {
+        Console.Write("Digite um numero inteiro: (digite 0 para sair) ");
+        valorUsuario = Convert.ToInt32(Console.ReadLine());
+
+        if (valorUsuario != 0)
+        {
+          if (valorUsuario % 2 == 0)
+          {
+            Console.WriteLine("O número " + valorUsuario + " é par.\n");
+          }
+          else
+          {
+            Console.WriteLine("O número " + valorUsuario + " é ímpar.\n");
+          }
+        }
+      } while (valorUsuario != 0);
+
+      Console.WriteLine("\nSaindo...");
+    }
+  }
+}
