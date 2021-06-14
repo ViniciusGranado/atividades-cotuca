a44aaf8bcabae5f11975901681b8b742171831d8
Vinicius Granado <vinicius.r.granado@gmail.com>
2021-06-14 17:33:43 -0300

    Added exercicio03

---
+++ semestre-1/tecnicas-de-programacao/avaliacao02/exercicio03/exercicio03/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace exercicio03
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int valorUsuario;
+
+      Console.Write("Digite um valor inteiro: ");
+      valorUsuario = int.Parse(Console.ReadLine());
+
+      Console.WriteLine(); // Pula linha
+
+      for (int i = 1; i < valorAbsoluto(valorUsuario); i++)
+      {
+        Console.WriteLine(i);
+      };
+
+      int valorAbsoluto (int valor)
+      {
+        if (valor >= 0)
+        {
+          return valor;
+        }
+
+        return valor - valor - valor;
+      }
+    }
+  }
+}
+
