14909692bf64d0feb3021418db2f475cfa60963d
Vinicius Granado <vinicius.r.granado@gmail.com>
2021-06-14 17:08:24 -0300

    Added exercicio02

---
+++ semestre-1/tecnicas-de-programacao/avaliacao02/exercicio02/exercicio02/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace exercicio02
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      String nomeUsuario;
+      double salario;
+      double desconto;
+      double valorDesconto;
+      double salarioComDesconto;
+
+      do
+      {
+        Console.Write("Digite o seu nome: (deixe em branco para sair) ");
+        nomeUsuario = Console.ReadLine();
+
+        if (nomeUsuario != "")
+        {
+          Console.Write("Digite o seu salário: R$");
+          salario = double.Parse(Console.ReadLine());
+
+          if (salario <= 500)
+          {
+            desconto = 0;
+          } 
+          else if (salario <= 1000)
+          {
+            desconto = 5;
+          }
+          else
+          {
+            desconto = 10;
+          };
+
+          valorDesconto = salario * desconto / 100;
+          salarioComDesconto = salario - valorDesconto;
+
+
+          Console.WriteLine("\nFuncionário: " + nomeUsuario);
+          Console.WriteLine(
+            desconto == 0 ?
+              "Funcionário isento de descontos." :
+              "Total de descontos R$" + valorDesconto
+          );
+          Console.WriteLine("Salário final: R$" + salarioComDesconto + "\n");
+        }
+      } while (nomeUsuario != "");
+
+      Console.WriteLine("\nSaindo...");
+    }
+  }
+}
