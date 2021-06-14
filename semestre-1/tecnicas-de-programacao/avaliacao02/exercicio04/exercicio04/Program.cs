0b6b1b7d43a8009efbf070e48dc3f75661aec917
Vinicius Granado <vinicius.r.granado@gmail.com>
2021-06-14 17:35:49 -0300

    Added exercicio04

---
+++ semestre-1/tecnicas-de-programacao/avaliacao02/exercicio04/exercicio04/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace exercicio04
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      for (int i = 10; i <=20; i++)
+      {
+        Console.WriteLine(i);
+      }
+    }
+  }
+}
