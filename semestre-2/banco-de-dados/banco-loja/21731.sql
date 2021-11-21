0de4dac00238862ab621eb64a1a6bfa2d3708724
Vinicius Granado <vinicius.r.granado@gmail.com>
2021-11-20 22:08:11 -0300

    Update Loja queries

--- semestre-2/banco-de-dados/banco-loja/21731.sql
+++ semestre-2/banco-de-dados/banco-loja/21731.sql
@@ -1,4 +1,4 @@
--- LISTA FINAL DE EXERCÕCIOS
+-- LISTA FINAL DE EXERC√çCIOS
 -- VINICIUS RAFAEL GRANADO
 -- RA: 21731
 
@@ -14,4 +14,108 @@ SELECT produto.id, produto.nome FROM itempedido
 INNER JOIN produto
 ON itempedido.id_prod = produto.id
 GROUP BY produto.id, produto.nome
-HAVING COUNT(produto.nome) = 0
+HAVING COUNT(produto.id) = 0;
+
+-- 3
+SELECT id_ped, COUNT(id_prod) AS totalItensPedido FROM itempedido
+GROUP BY id_ped;
+
+-- 4
+SELECT produto.id, produto.nome, SUM(itempedido.qtd) AS qtdPedidos FROM itempedido
+INNER JOIN produto
+ON itempedido.id_prod = produto.id
+GROUP BY produto.id, produto.nome;
+
+-- 5
+SELECT id_prod, nome, (valor * quantidade) AS valorTotal FROM (
+	SELECT id_prod, itempedido.valor, SUM(itempedido.qtd) AS quantidade, produto.nome FROM itempedido
+	INNER JOIN produto
+	ON itempedido.id_prod = produto.id
+	GROUP BY itempedido.id_prod, itempedido.valor, produto.nome
+) AS quantidadePorProduto
+ORDER BY id_prod;
+
+-- 6
+CREATE VIEW valorTotalPorPedido AS
+SELECT id_ped AS idPedido, SUM(qtd * valor) AS valorTotalPedido FROM itempedido
+GROUP BY id_ped
+
+SELECT idPedido, valorTotalPedido from valorTotalPorPedido
+WHERE valorTotalPedido = (
+	SELECT MAX(valorTotalPedido) from valorTotalPorPedido
+);
+
+-- 7
+SELECT cliente.id, cliente.nome, COUNT(cliente.id) AS totalPedidos FROM pedido
+INNER JOIN cliente
+ON pedido.id_cli = cliente.id
+GROUP BY cliente.id, cliente.nome
+
+-- 8
+SELECT id_ped AS idPedido, SUM(qtd * valor) AS valorTotalPedido FROM itempedido
+GROUP BY id_ped
+
+-- 9
+SELECT valorTotalPorPedido.idPedido, valorTotalPorPedido.valorTotalPedido from (
+	SELECT id_ped AS idPedido, SUM(qtd * valor) AS valorTotalPedido FROM itempedido
+	GROUP BY id_ped
+) AS valorTotalPorPedido
+WHERE (valorTotalPedido = (
+	SELECT MAX(valorTotalPedido) from valorTotalPorPedido
+)) OR (valorTotalPedido = (
+	SELECT MIN(valorTotalPedido) from valorTotalPorPedido
+));
+
+-- 10
+SELECT cliente.id, cliente.nome, COUNT(cliente.id) AS totalPedidos FROM pedido
+INNER JOIN cliente
+ON pedido.id_cli = cliente.id
+GROUP BY cliente.id, cliente.nome
+
+-- 11
+
+-- 12
+
+-- 13
+CREATE PROCEDURE cadastraAtualizaCliente (
+	@nome VARCHAR(30),
+	@cidade VARCHAR(30),
+	@id INT = -1
+) AS
+	SET NOCOUNT ON
+	IF EXISTS(SELECT 1 FROM cliente WHERE nome = @nome OR id = @id)
+	BEGIN
+		UPDATE cliente
+		SET nome = @nome,
+			cidade = @cidade
+		WHERE nome = @nome OR id = @id;
+
+		PRINT 'Cliente atualizado com sucesso';
+	END
+	ELSE
+		BEGIN
+			INSERT INTO cliente VALUES(@nome, @cidade, getdate());
+			PRINT 'Cliente inserido com sucesso';
+		END
+
+--14
+CREATE PROCEDURE valorTotalDeVendaPorIdProduto (
+	@id int
+) AS 
+	SELECT id_prod, nome, (valor * quantidade) AS valorTotal FROM (
+		SELECT id_prod, itempedido.valor, SUM(itempedido.qtd) AS quantidade, produto.nome FROM itempedido
+		INNER JOIN produto
+		ON itempedido.id_prod = produto.id
+		GROUP BY itempedido.id_prod, itempedido.valor, produto.nome
+	) AS quantidadePorProduto
+	WHERE id_prod = @id;
+
+-- 15
+CREATE PROCEDURE totalPedidosPorIdProduto (
+	@id int
+) AS 
+	SELECT produto.id, produto.nome, SUM(itempedido.qtd) AS qtdPedidos FROM itempedido
+	INNER JOIN produto
+	ON itempedido.id_prod = produto.id
+	WHERE produto.id = @id
+	GROUP BY produto.id, produto.nome
--- semestre-2/banco-de-dados/banco-loja/loja.sql
+++ semestre-2/banco-de-dados/banco-loja/loja.sql
@@ -2,7 +2,7 @@ CREATE DATABASE Loja;
 --DROP DATABASE Loja;
 GO
 
-USE Loja;
+
 
 create table cliente (
 	id int not null identity(1,1) PRIMARY KEY,
@@ -11,16 +11,16 @@ create table cliente (
 	dt_cadastro datetime not null default getdate()
 );
 
-INSERT INTO cliente VALUES('Jo„o dos Santos', 'S„o Paulo', getdate());
+INSERT INTO cliente VALUES('Jo√£o dos Santos', 'S√£o Paulo', getdate());
 INSERT INTO cliente VALUES('Kieza Valadim ', 'Campinas', getdate());
-INSERT INTO cliente VALUES('JÈnifer Lacerda Mondrag„o', 'Belo Horizonte', getdate());
-INSERT INTO cliente VALUES('Mellyssa Semedo Abrantes', 'S„o Paulo', getdate());
-INSERT INTO cliente VALUES('Zaqueu Galv„o CaniÁa', 'Belo Horizonte', getdate());
+INSERT INTO cliente VALUES('J√©nifer Lacerda Mondrag√£o', 'Belo Horizonte', getdate());
+INSERT INTO cliente VALUES('Mellyssa Semedo Abrantes', 'S√£o Paulo', getdate());
+INSERT INTO cliente VALUES('Zaqueu Galv√£o Cani√ßa', 'Belo Horizonte', getdate());
 INSERT INTO cliente VALUES('Filipe Albernaz Janes', 'Valinhos', getdate());
-INSERT INTO cliente VALUES('Iara Gois Anhaia', 'JundiaÌ', getdate());
+INSERT INTO cliente VALUES('Iara Gois Anhaia', 'Jundia√≠', getdate());
 INSERT INTO cliente VALUES('Nikol Lameira Teles', 'Campinas', getdate());
-INSERT INTO cliente VALUES('Vanessa Carvalhosa Vergueiro', 'PaulÌnia', getdate());
-INSERT INTO cliente VALUES('Michele Sanches Cambezes', 'CosmÛpolis', getdate());
+INSERT INTO cliente VALUES('Vanessa Carvalhosa Vergueiro', 'Paul√≠nia', getdate());
+INSERT INTO cliente VALUES('Michele Sanches Cambezes', 'Cosm√≥polis', getdate());
 
 SELECT * FROM cliente;
 
@@ -62,7 +62,7 @@ create table produto (
 	qtd int default 0
 );
 
-INSERT INTO produto VALUES ('L·pis', 'L·pis', getdate(), 1.45, 20);
+INSERT INTO produto VALUES ('L√°pis', 'L√°pis', getdate(), 1.45, 20);
 INSERT INTO produto VALUES ('Garrafa', 'Garrafa', getdate(), 20.99, 14);
 INSERT INTO produto VALUES ('Copo', 'Copo', getdate(), 1.99, 35);
 INSERT INTO produto VALUES ('Livro', 'Livro', getdate(), 35.5, 12);
@@ -82,45 +82,42 @@ create table itempedido (
 	valor float not null
 );
 
-INSERT INTO itempedido VALUES(1, 7, 2, 179.98);
+INSERT INTO itempedido VALUES(1, 7, 2, 89.99);
 INSERT INTO itempedido VALUES(2, 4, 1, 35.50);
 INSERT INTO itempedido VALUES(2, 5, 1, 1299.99);
 INSERT INTO itempedido VALUES(2, 6, 1, 22.99);
 INSERT INTO itempedido VALUES(3, 3, 1, 1.99);
-INSERT INTO itempedido VALUES(3, 5, 3, 3899.97);
+INSERT INTO itempedido VALUES(3, 5, 3, 1299.99);
 INSERT INTO itempedido VALUES(3, 8, 1, 22.35);
 INSERT INTO itempedido VALUES(3, 9, 1, 0.99);
-INSERT INTO itempedido VALUES(4, 9, 2, 1.98);
+INSERT INTO itempedido VALUES(4, 9, 2, 0.99);
 INSERT INTO itempedido VALUES(5, 3, 1, 1.99);
 INSERT INTO itempedido VALUES(5, 6, 1, 22.99);
-INSERT INTO itempedido VALUES(6, 2, 2, 41.98);
+INSERT INTO itempedido VALUES(6, 2, 2, 20.99);
 INSERT INTO itempedido VALUES(6, 4, 1, 35.50);
-INSERT INTO itempedido VALUES(6, 5, 3, 3899.97);
+INSERT INTO itempedido VALUES(6, 5, 3, 1299.99);
 INSERT INTO itempedido VALUES(7, 7, 1, 89.99);
 INSERT INTO itempedido VALUES(8, 1, 1, 1.45);
 INSERT INTO itempedido VALUES(9, 3, 1, 1.99);
-INSERT INTO itempedido VALUES(9, 8, 4, 89.40);
+INSERT INTO itempedido VALUES(9, 8, 4, 22.35);
 INSERT INTO itempedido VALUES(9, 9, 1, 0.99);
 INSERT INTO itempedido VALUES(10, 6, 1, 22.99);
-INSERT INTO itempedido VALUES(11, 5, 2, 2599.98);
-INSERT INTO itempedido VALUES(11, 9, 2, 1.98);
+INSERT INTO itempedido VALUES(11, 5, 2, 1299.99);
+INSERT INTO itempedido VALUES(11, 9, 2, 0.99);
 INSERT INTO itempedido VALUES(12, 2, 1, 20.99);
-INSERT INTO itempedido VALUES(12, 4, 6, 142);
+INSERT INTO itempedido VALUES(12, 4, 6, 35.50);
 INSERT INTO itempedido VALUES(13, 2, 1, 20.99);
-INSERT INTO itempedido VALUES(13, 8, 4, 89.40);
+INSERT INTO itempedido VALUES(13, 8, 4, 22.35);
 INSERT INTO itempedido VALUES(13, 9, 1, 0.99);
-INSERT INTO itempedido VALUES(14, 3, 2, 3.98);
+INSERT INTO itempedido VALUES(14, 3, 2, 1.99);
 INSERT INTO itempedido VALUES(14, 7, 1, 89.99);
-INSERT INTO itempedido VALUES(14, 8, 2, 44.70);
+INSERT INTO itempedido VALUES(14, 8, 2, 22.35);
 INSERT INTO itempedido VALUES(15, 9, 1, 0.99);
 INSERT INTO itempedido VALUES(16, 3, 1, 1.99);
-INSERT INTO itempedido VALUES(16, 7, 6, 539.94);
+INSERT INTO itempedido VALUES(16, 7, 6, 89.99);
 INSERT INTO itempedido VALUES(16, 7, 1, 89.99);
-INSERT INTO itempedido VALUES(17, 1, 4, 5.80);
+INSERT INTO itempedido VALUES(17, 1, 4, 1.45);
 INSERT INTO itempedido VALUES(17, 8, 1, 22.35);
-INSERT INTO itempedido VALUES(19, 2, 2, 41.98);
+INSERT INTO itempedido VALUES(19, 2, 2, 20.99);
 INSERT INTO itempedido VALUES(20, 6, 1, 22.99);
 INSERT INTO itempedido VALUES(20, 8, 1, 22.35);
-
-SELECT * FROM itempedido;
-
