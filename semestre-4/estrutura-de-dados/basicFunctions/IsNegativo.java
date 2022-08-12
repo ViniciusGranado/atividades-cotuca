3b1071335ce001989cee320f3d09a2ff7de1253c
Vinicius Granado <vinicius.r.granado@gmail.com>
2022-08-11 21:45:56 -0300

    Added sum function

--- semestre-4/estrutura-de-dados/basicFunctions/IsNegativo.java
+++ semestre-4/estrutura-de-dados/basicFunctions/IsNegativo.java
@@ -1,17 +1,33 @@
 public class IsNegativo {
-    public static void main(String[] args) {
-        System.out.println(isNegativo(-5));
-        System.out.println(isNegativo(5));
-    };
+  public static void main(String[] args) {
+    System.out.println(isNegativo(-5));
+    System.out.println(isNegativo(5));
     
-    static boolean isNegativo(int n) {
-        return isNegativo(n, n);
-    };
-    
-    static boolean isNegativo(int a, int d) {
-        if (a == -1) return true;
-        if (d == 1) return false;
-        
-        return isNegativo(a + 1, d - 1);
+    System.out.println(sum(-5, 8));
+  };
+  
+  static boolean isNegativo(int n) {
+    return isNegativo(n, n);
+  };
+
+  static int sum(int n, int m) {
+    if (m == 0) {
+      return n;
     }
+
+    if (isNegativo(m)) {
+      return sum(n - 1, m + 1);
+    }
+
+    return sum(n + 1, m - 1);
+  };
+  
+  //////////////
+
+  static boolean isNegativo(int a, int d) {
+    if (a == -1) return true;
+    if (d == 1) return false;
+    
+    return isNegativo(a + 1, d - 1);
+  }
 }
