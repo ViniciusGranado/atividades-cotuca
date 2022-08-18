7f57e9d0763afe73fc4deba8eb02d3f9493cdc7c
Vinicius Granado <vinicius.r.granado@gmail.com>
2022-08-18 19:57:40 -0300

    Added greaterThan function

--- semestre-4/estrutura-de-dados/basicFunctions/BaicFunctions.java
+++ semestre-4/estrutura-de-dados/basicFunctions/BaicFunctions.java
@@ -1,25 +1,35 @@
 public class BaicFunctions {
   public static void main(String[] args) {
+    // isNegativo()
     System.out.println(isNegativo(-5));
     System.out.println(isNegativo(5));
     
+    // sum()
     System.out.println(sum(-5, 8));
     System.out.println(sum(5, -8));
     
+    // minus()
     System.out.println(minus(5, 3));
     System.out.println(minus(-10, 5));
     System.out.println(minus(10, -5));
     System.out.println(minus(-10, -5));
+    
+    // greaterThan()
+    System.out.println(greaterThan(5, 2));
+    System.out.println(greaterThan(2 , 10));
+    System.out.println(greaterThan(-2, -5));
+    System.out.println(greaterThan(-5, -2));
+    System.out.println(greaterThan(2, 2));
   };
   
   static boolean isNegativo(int n) {
+    if (n == 0) return false;
+
     return isNegativo(n, n);
   };
 
   static int sum(int n, int m) {
-    if (m == 0) {
-      return n;
-    }
+    if (m == 0) return n;
 
     if (isNegativo(m)) {
       return sum(n - 1, m + 1);
@@ -29,15 +39,27 @@ public class BaicFunctions {
   };
   
   static int minus(int n, int m) {
-    if (m == 0) {
-      return n;
-    }
+    if (m == 0) return n;
 
     if (isNegativo(m)) {
       return minus(n + 1, m + 1);
     }
 
     return minus(n - 1, m - 1);
+  }
+  
+  static boolean greaterThan(int n, int m) {
+    if (m == n) return false;
+
+    if (m == 0) {
+        return !isNegativo(n);
+    }
+    
+    if (isNegativo(m)) {
+        return greaterThan(++n, ++m);
+    }
+
+    return greaterThan(--n, --m);
   }
   
   //////////////
