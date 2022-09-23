6dcbc84414150bf66286afe6aae9a1f12e3914e3
Vinicius Granado <vinicius.r.granado@gmail.com>
2022-09-23 19:53:05 -0300

    Allowed CORS

--- semestre-4/desenvolvimento-web/ProjetoEscola_API/Program.cs
+++ semestre-4/desenvolvimento-web/ProjetoEscola_API/Program.cs
@@ -2,7 +2,23 @@ using Microsoft.EntityFrameworkCore;
 using ProjetoEscola_API.Data;
 var builder = WebApplication.CreateBuilder(args);
 
+//Allow CORS
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 // Add services to the container.
+
+//Allow CORS
+builder.Services.AddCors(options =>
+{
+  options.AddPolicy(MyAllowSpecificOrigins, builder =>
+  {
+    builder.WithOrigins("http://localhost").AllowAnyOrigin().AllowAnyMethod().Allo
+  wAnyHeader();
+    builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+    builder.SetIsOriginAllowed(origin => new Uri(origin).Host == "localhost");
+    builder.SetIsOriginAllowed(origin => true);
+  });
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -24,7 +40,10 @@ if (app.Environment.IsDevelopment())
   app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
+
+//Allow CORS
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseAuthorization();
 
