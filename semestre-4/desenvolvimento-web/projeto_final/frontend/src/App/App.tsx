e0a57b27fe291afb35d7ad9818fa34b78505bef5
Vinicius Granado <vinicius.r.granado@gmail.com>
2022-10-21 19:53:43 -0300

    Update initial project

---
+++ semestre-4/desenvolvimento-web/projeto_final/frontend/src/App/App.tsx
@@ -0,0 +1,13 @@
+import { Typography } from '@mui/material';
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Typography>App</Typography>
+    </>
+  );
+}
+
+export default App;
