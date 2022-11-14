d37b2c08236bf9b7c3480fca2802aa768f9ab3a6
Vinicius Granado <vinicius.r.granado@gmail.com>
2022-11-14 16:03:54 -0300

    update Product model

--- semestre-4/desenvolvimento-web/projeto_final/backend/Models/Product.cs
+++ semestre-4/desenvolvimento-web/projeto_final/backend/Models/Product.cs
@@ -7,5 +7,6 @@ namespace backend.Models
     public string description { get; set; }
     public string imgUrl { get; set; }
     public float price { get; set; }
+    public int userId { get; set; }
   }
 }
